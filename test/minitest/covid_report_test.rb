acc88da34db45424d16cba9f09c892d4a8ce7f9b
Chris Meono <chrismeono1022@gmail.com>
2024-10-25 17:54:14 -0700

    Add fixture for location key lookup for accuweather api

--- test/minitest/covid_report_test.rb
+++ test/minitest/covid_report_test.rb
@@ -1,8 +1,38 @@
 require 'pry'
 require 'minitest/autorun'
 
+require 'covid_report'
+
 class CovidReportTest < Minitest::Test
-  def test_i_run
-    assert_equal true, true
+  def setup
+    @state_level_data = JSON.parse(File.read('test/fixtures/cdc_covid_state_overview_endpoint.json'), symbolize_names: true)
+    @detailed_state_data = JSON.parse(File.read('test/fixtures/cdc_covid_state_detailed_endpoint.json'), symbolize_names: true)
+    @variant_data = JSON.parse(File.read('test/fixtures/cdc_covid_variant_endpoint.json'), symbolize_names: true)
+  end
+
+  def test_create_covid_report_with_stub
+    covid = CovidReport.new
+
+    # stub API calls
+    api_stubs = ->(uri) {
+      if uri == CovidReport::STATE_LEVEL_DATA_URL
+        @state_level_data
+      elsif uri == CovidReport::CIRCULATING_VARIANTS_URL
+        @variant_data
+      elsif uri == CovidReport::COMPARISON_DATA_URL
+        @detailed_state_data
+      end
+    }
+
+    covid.stub :fetch, api_stubs do
+      covid.create_covid_report
+
+      # binding.pry
+
+      assert_equal 'Very High', covid.state_data.label
+      assert_equal '10', covid.state_data.level
+
+
+    end
   end
 end
